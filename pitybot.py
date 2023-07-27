d6db4227a50889130c01244b274042f40a5a106d
skglw <skglvv@gmail.com>
2023-07-27 09:13:31 +0300

    triggers + stickers updated

--- pitybot.py
+++ pitybot.py
@@ -18,6 +18,18 @@ def get_sticker():
         'CAACAgIAAxkBAAI4vmTB3TJF6c5PTszPlNyCwUqn703LAAKEEwACOW7JS2ZVTlSwaykPLwQ',
         'CAACAgIAAxkBAAI4wGTB3TRGoawqDHGvv6OY5jqvpBwUAALtEgACgCTIS7ELWSqTwy0YLwQ',
         'CAACAgIAAxkBAAI4wmTB3UTLatuNIkF3HzHPko2vM3iEAALzFwACqkdJSC8ry4607EHCLwQ',
+        'CAACAgIAAxkBAAI4xmTCCVgYgj_vo3bYFZmWwxUCHQ_2AAJzMwACJXIRSoJM6cbVizIiLwQ',
+        'CAACAgIAAxkBAAI4yGTCCVpyxsw7K98YMOFpP4qA4d_RAAJ9NAACrb8QStpkLcCm9PDyLwQ',
+        'CAACAgIAAxkBAAI4ymTCCV1osUc1W6QX2OcpoDVn6R3NAAIEAAOEGRA_oqueBwRymoYvBA',
+        'CAACAgIAAxkBAAI4zGTCCXezXFSiN1UhjMFAdFY3IOqeAALTJwAC0QFJS6Lbwo1Vs0qsLwQ',
+        'CAACAgIAAxkBAAI4zmTCCXimGnqcK_LvZLZMHhO32koIAAJTKgACPSNJS-RST1h7Cb9CLwQ',
+        'CAACAgQAAxkBAAI40GTCCZoFiOu7UkOiy_37EN-KcZoBAALSAgACj7NZA06V6kbKvFjnLwQ',
+        'CAACAgIAAxkBAAI40mTCCdBz7ZlJWRWWQYATlSMr9RrXAAJ7EwACYC4pSNF5jt3SitAMLwQ',
+        'CAACAgIAAxkBAAI41GTCCeu4Arr2Wap-95RKw45_IHEiAAI_GgACwEZpSRPI-nLhMLoQLwQ',
+        'CAACAgIAAxkBAAI41mTCCfRhT0p4OIr0XsQOfp01vcJUAAIqEwACoL74S74mGdF3Gg6ZLwQ',
+        'CAACAgIAAxkBAAI42GTCCggbFHtSeI-tlgyVZ7aAPIoHAAK8DwACk1r5S2NA-mslGBz3LwQ',
+        'CAACAgIAAxkBAAI42mTCCkWNkuVngQdB3K-7GV16YNG2AAJhAgAC81Y_FxQ-kB2ef5bKLwQ',
+        'CAACAgIAAxkBAAI43GTCCk3TsC57mNkU54vakiSP69fuAAKGAgAC81Y_F7AEphqEvu5_LwQ',
         'CAACAgIAAxkBAAI4xGTB3Uo0Mh-lrITWoOE2oBq6HX-YAAKMFAACGndJSHhnyYts9sZYLwQ'
     ]
     return random.choice(stickers)
@@ -25,19 +37,25 @@ def get_message():
     messages = [
         'нет мы все вместе',
         'нет бро мы с тобой',
-        'нет мы все тоже'
+        'нет мы все тоже',
+        'а как же мы(((',
+        'в этой комнате??'
     ]
     return random.choice(messages)
 
 def is_user_alone(message):
     message = message.lower()
     im_alone = [
+        'яя один',
+        'я один.',
         'я один',
         'я один?',
+        'яя один?',
         'я один??',
         'я один..?',
         'я один?..',
         'я одна',
+        'я одна.',
         'я одна?',
         'я одна??',
         'я одна..?',
@@ -53,16 +71,23 @@ def is_user_alone(message):
         'интересно я одна??',
         'интересно я одна?..'
     ]
+    whos_alone = [
+        '/кто один',
+        'кто один?',
+        'кто один',
+        'кто один то',
+        'да кто один то'
+    ]
     if message in im_alone:
         return {'res': True, 'mes': get_message(), 'pic': get_sticker()}
     
-    elif message == '/кто один' or message == 'кто один?'or message == 'кто один':
+    elif message in whos_alone:
         return {'res': True, 'mes': 'никто не один бро', 'pic': get_sticker()}
     
-    elif message == 'я не один?' or message == 'я не один??':
+    elif message == 'я не один?' or message == 'я не один??' or message == 'я не один' or message == 'я не один.':
         return {'res': True, 'mes': 'да бро ты не один', 'pic': get_sticker()}
     
-    elif message == 'я не одна?' or message == 'я не одна??':
+    elif message == 'я не одна?' or message == 'я не одна??' or message == 'я не одна' or message == 'я не одна.':
         return {'res': True, 'mes': 'да любимка ты не одна', 'pic': get_sticker()}
     
     else:
